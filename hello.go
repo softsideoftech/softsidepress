1bd5c3207a14c9ac8cbb5ccd36d7ec93b3d334b5
vlad <vgiverts@gmail.com>
2017-11-14 16:03:15 +0100

    Proof of concept sending markdown to html emails via SES.

--- hello.go
+++ hello.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-        "fmt"
-        "github.com/sourcegraph/go-ses"
+	"fmt"
+	"github.com/sourcegraph/go-ses"
+	"gopkg.in/russross/blackfriday.v2"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
 
-    // Change the From address to a sender address that is verified in your Amazon SES account.
-    from := "vladgiverts@softsideoftech.com"
-    to := "vladgiverts@softsideoftech.com"
+	markdownEmailBody, err := ioutil.ReadFile("/Users/vlad/go/src/hello/testemail.txt")
 
-    // EnvConfig uses the AWS credentials in the environment variables $AWS_ACCESS_KEY_ID and
-    // $AWS_SECRET_KEY.
-    res, err := ses.EnvConfig.SendEmail(from, to, "Hello, world!", "Here is the message body.")
-    if err == nil {
-        fmt.Printf("Sent email: %s...\n", res[:32])
-    } else {
-        fmt.Printf("Error sending email: %s\n", err)
-    }
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", err)
+		os.Exit(1)
+	}
+
+	htmlEmailBytes := blackfriday.Run(markdownEmailBody)
+	htmlEmailString := string(htmlEmailBytes)
+
+	fmt.Print(htmlEmailString)
+
+	// Change the From address to a sender address that is verified in your Amazon SES account.
+	from := "vladgiverts@softsideoftech.com"
+	to := "vladgiverts@softsideoftech.com"
+
+	// EnvConfig uses the AWS credentials in the environment variables $AWS_ACCESS_KEY_ID and
+	// $AWS_SECRET_KEY.
+	res, err := ses.EnvConfig.SendEmailHTML(from, to, "Hello, world 2", string(markdownEmailBody), htmlEmailString)
+	if err == nil {
+		fmt.Printf("Sent email: %s...\n", res[:32])
+	} else {
+		fmt.Printf("Error sending email: %s\n", err)
+	}
 }
