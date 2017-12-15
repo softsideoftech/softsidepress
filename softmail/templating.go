105c5eb945dfd949f4e9d5f249394dc1d876a28e
vlad <vgiverts@gmail.com>
2017-12-15 11:45:55 +0100

    Added caching and html escaping to templates

--- softmail/templating.go
+++ softmail/templating.go
@@ -1,11 +1,13 @@
 package softmail
 
 import (
-	"text/template"
+	txtTemplate "text/template"
+	htmlTemplate "html/template"
 	"io"
 	"bytes"
 	"gopkg.in/russross/blackfriday.v2"
 	"io/ioutil"
+	"sync"
 )
 
 type HtmlPageParams struct {
@@ -22,34 +24,43 @@ type BodyParams interface {
 const cssFile = "/Users/vlad/go/src/softside/style.css" // TODO: make this a relative path
 var cssFileBytes, err = ioutil.ReadFile(cssFile)
 var cssFileString = string(cssFileBytes)
-
+var templateCache = sync.Map{}
 
 // TODO: figure out how to load and cache all the template files
-func renderMarkdownToHtmlTemplate(writer io.Writer, templateFile string, title string, bodyFile string, bodyParams BodyParams) error {
+func renderMarkdownToHtmlTemplate(writer io.Writer, baseHtmlFile string, title string, markdownFile string, bodyParams BodyParams) error {
+	templateName := baseHtmlFile + markdownFile
 
-	// Load the markdown template file
-	markdownTemplateBytes, err := ioutil.ReadFile(bodyFile)
-	if err != nil {
-		return err
+	// Get the template from the cache to avoid constantly reading and parsing files from disk
+	fullPageTemplate, ok := templateCache.Load(templateName)
+	if !ok {
+		// Load the markdown template file
+		markdownTemplateBytes, err := ioutil.ReadFile(markdownFile)
+		if err != nil {
+			return err
+		}
+
+		// Render the markdown as HTML
+		bodyHtml := string(blackfriday.Run(markdownTemplateBytes))
+
+		// Merge the markdown html into the base
+		baseHtmlTemplate, err := txtTemplate.ParseFiles(baseHtmlFile)
+		if err != nil {
+			return err
+		}
+		buffer := &bytes.Buffer{}
+		baseHtmlTemplate.Execute(buffer, HtmlPageParams{Title: title, Css: cssFileString, Body: bodyHtml})
+
+		// Render the parameters into the full template.
+		// Using an html template instead of text to safely escape user-passed params
+		fullPageTemplate, err = htmlTemplate.New(templateName).Parse(buffer.String())
+		if err != nil {
+			return err
+		}
+		templateCache.Store(templateName, fullPageTemplate)
 	}
 
-	// Render the markdown as HTML
-	bodyHtml := string(blackfriday.Run(markdownTemplateBytes))
-
-	// Templatize the markdown body
-	bodyMarkdownTemplate, err := template.New(bodyFile).Parse(bodyHtml)
-	if err != nil {
-		return err
-	}
-	buffer := &bytes.Buffer{}
-	bodyMarkdownTemplate.Execute(buffer, bodyParams)
-
-	// Write out the html
-	htmlTemplate, err := template.ParseFiles(templateFile)
-	if err != nil {
-		return err
-	}
-	htmlTemplate.Execute(writer, HtmlPageParams{Title: title, Css: cssFileString, Body: buffer.String()})
+	template := fullPageTemplate.(*htmlTemplate.Template)
+	template.Execute(writer, bodyParams)
 
 	return nil
 }
