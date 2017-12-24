72753c7d1a91299cdf06b720cc4b436171b4d65f
vlad <vgiverts@gmail.com>
2017-12-24 16:35:35 +0100

    Created init.d service script

---
+++ softside_service.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides: softside
+# Required-Start: $syslog
+# Required-Stop: $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: SoftSideOfTech
+# Description: This file starts and stops the Soft Side of Tech site/service.
+#
+### END INIT INFO
+
+
+case "$1" in
+ start)
+   /root/.bash_profile
+   cd /root/go
+   go run src/softside/hello.go & echo $! > /root/go/softside.pid
+   ;;
+ stop)
+   /usr/bin/killall -s 9 hello
+   ;;
+ restart)
+   /usr/bin/killall -s 9 hello
+  source /root/.bash_profile
+  cd /root/go
+  go run src/softside/hello.go & echo $! > /root/go/softside.pid
+   ;;
+ *)
+   echo "Usage: tomcat {start|stop|restart}" >&2
+   exit 3
+   ;;
+esac
