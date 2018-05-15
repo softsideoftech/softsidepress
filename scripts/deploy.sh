905c3f13532365f6026243d062a7704e5cc4c85e
vlad <vgiverts@gmail.com>
2018-05-15 16:08:11 +0200

    Fixing circleci config.yml (attempt 4)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-cd ~/go/src/softside/
-gzip softside
-scp softside.gz root@softsideoftech.com:
+gzip /tmp/softside
+scp /tmp/softside.gz root@softsideoftech.com:
 ssh root@softsideoftech.com 'gunzip -f softside.gz'
 ssh root@softsideoftech.com 'service softside restart'
